822ea48d959b6ff24828f19bebd743f36d01af60
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-06-30 22:44:18 +0200

    Added the simplest instrumentation test. Apply napalm driven design to the rest

--- app/src/androidTest/java/es/voghdev/prjdagger2/ApplicationTest.java
+++ app/src/androidTest/java/es/voghdev/prjdagger2/ApplicationTest.java
@@ -1,6 +1,5 @@
 package es.voghdev.prjdagger2;
 
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -9,11 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import es.voghdev.prjdagger2.global.App;
-import es.voghdev.prjdagger2.global.di.RootComponent;
-import es.voghdev.prjdagger2.global.di.UserListModule;
 import es.voghdev.prjdagger2.ui.activity.UserListActivity;
-import it.cosenonjaviste.daggermock.DaggerMockRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -22,22 +17,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class) @LargeTest
 public class ApplicationTest  {
-
-    @Rule
-    public DaggerMockRule<RootComponent> daggerRule =
-            new DaggerMockRule<>(RootComponent.class,
-                    new UserListModule((App) InstrumentationRegistry.getInstrumentation()
-                            .getTargetContext()
-                            .getApplicationContext())).set(
-                    new DaggerMockRule.ComponentSetter<RootComponent>() {
-                        @Override public void setComponent(RootComponent component) {
-                            App app =
-                                    (App) InstrumentationRegistry.getInstrumentation()
-                                            .getTargetContext()
-                                            .getApplicationContext();
-                            app.setComponent(component);
-                        }
-                    });
 
     @Rule public IntentsTestRule<UserListActivity> activityRule =
             new IntentsTestRule<>(UserListActivity.class, true, false);
