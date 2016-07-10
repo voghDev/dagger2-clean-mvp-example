67d512e9bf09129998d282f805395b9731a67d65
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-07-10 20:52:27 +0200

    Added empty cache policy

--- app/src/main/java/es/voghdev/prjdagger2/global/di/MainModule.java
+++ app/src/main/java/es/voghdev/prjdagger2/global/di/MainModule.java
@@ -21,17 +21,20 @@ import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
+import es.voghdev.prjdagger2.datasource.api.GetUsersApiImpl;
 import es.voghdev.prjdagger2.datasource.datafile.GetUsersFileImpl;
 import es.voghdev.prjdagger2.global.App;
 import es.voghdev.prjdagger2.interactor.GetUsersInteractor;
 import es.voghdev.prjdagger2.interactor.impl.MainThreadImpl;
 import es.voghdev.prjdagger2.interactor.impl.ThreadExecutor;
+import es.voghdev.prjdagger2.repository.UserRepository;
 
 @Module
 public class MainModule {
     private App application;
 
     GetUsersInteractor interactor;
+    UserRepository userRepository;
 
     public MainModule(App application) {
         this.application = application;
@@ -39,12 +42,17 @@ public class MainModule {
         interactor = new GetUsersInteractor(new GetUsersFileImpl(application),
                 new ThreadExecutor(),
                 new MainThreadImpl());
+
+        userRepository = new UserRepository(null, new GetUsersApiImpl(10, 0), new GetUsersFileImpl(application));
     }
 
     @Provides
     GetUsersInteractor provideUserInteractor(){
         return interactor;
     }
+
+    @Provides
+    UserRepository provideUserRepository() { return userRepository; }
 
     @Provides
     @Named("applicationContext")
