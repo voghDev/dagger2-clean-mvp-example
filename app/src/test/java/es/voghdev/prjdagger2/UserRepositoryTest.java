6127ada0fa541371c03380713ba1c074f8c8aaa9
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-11-03 18:47:44 +0100

    Add license headers

--- app/src/test/java/es/voghdev/prjdagger2/UserRepositoryTest.java
+++ app/src/test/java/es/voghdev/prjdagger2/UserRepositoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 Olmo Gallegos Hernández.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package es.voghdev.prjdagger2;
 
 import android.content.Context;
@@ -20,10 +35,6 @@ import es.voghdev.prjdagger2.usecase.GetUsers;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
-
-/**
- * Created by olmo on 2/11/16.
- */
 
 public class UserRepositoryTest extends BaseUnitTest {
 
